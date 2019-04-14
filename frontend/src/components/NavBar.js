0cc49fc2a58636756e1517563d0555efc49c14d7
Robert Abreu <robertabreu@pursuit.org>
2019-04-14 16:03:39 -0400

    Updated Navbar to match ask

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { Grid } from '@material-ui/core/';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Add from '@material-ui/icons/Add';
 
 const styles = {
@@ -20,6 +18,14 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  linkText: {
+    textDecoration: 'none',
+    color: 'white',
+  },
+  linkIcon: {
+    textDecoration: 'none',
+    color: 'black',
+  }
 };
 
 function NavBar(props) {
@@ -28,17 +34,43 @@ function NavBar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Link to='/'>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-          </Link>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            Movie App
-          </Typography>
-          <Link to='/add'>
-            <Add />
-          </Link>
+          <Grid container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item xs>
+              <Link to='/' className={classes.linkText}>
+                <Typography variant="h6" color="inherit" className={classes.grow} align='center'>
+                  Home
+                </Typography>
+              </Link>
+            </Grid>
+
+            <Grid item xs>
+              <Link to='/movies' className={classes.linkText}>
+                <Typography variant="body1" color="inherit" className={classes.grow} align='center'>
+                  All Movies
+                </Typography>
+              </Link>
+            </Grid>
+
+            <Grid item xs>
+              <Link to='/movies/byGenre' className={classes.linkText}>
+                <Typography variant="body1" color="inherit" className={classes.grow} align='center'>
+                  By Genre
+                </Typography>
+              </Link>
+            </Grid>
+
+            <Grid item xs>
+              <Link to='/add' className={classes.linkIcon}>
+                <Typography align='center'>
+                  <Add />
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>

85e2cbba269f66d2100b758a9adad50cfff292f3
Robert Abreu <robertabreu@pursuit.org>
2019-04-14 16:11:48 -0400

    added base component the renders at home

---
+++ frontend/src/components/Base.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@material-ui/core/';
+export default (props => {
+    return (
+        <Typography
+            variant='h1'
+            align='center'
+            gutterBottom={true}
+        >
+            Welcome to MovieApp
+        </Typography>
+    )
+});
